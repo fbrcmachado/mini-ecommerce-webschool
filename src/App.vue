56ee1d27a3fb0a9f49a08a9d67965522b3ba6ba7
Fabricio Machado <fabricio.s.machado@outlook.com>
2017-08-12 22:06:42 -0300

    add vue material

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,29 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <CctesteComponent></CctesteComponent>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import CctesteComponent from './components/testeComponent.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'testeComponent',
+    components: {
+      CctesteComponent
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/testeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Meu primeiro componente em VueJS</div>
+</template>
+<script>
+    export default {
+
+    }
+
+</script>
+<style>
+    h1 {
+        font-weight: normal;
+        font-size: 50px;
+        font-weight: bold;
+        color: #f00;
+    }
+</style>
