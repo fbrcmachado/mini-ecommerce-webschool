4ecee27f23b0ce96320dc08387caff640ae59c0f
Fabricio Machado <fabricio.s.machado@outlook.com>
2017-08-12 23:44:15 -0300

    adição de menus e rotas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="phone-viewport">
+  <main class="phone-viewport">
     <md-toolbar>
-      <h1 class="md-title">Ecommerce Webschool</h1>
+      <md-button class="md-icon button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">{{ title }}</h2>
     </md-toolbar>
-  </div>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Navegação</h3>
+        </div>
+      </md-toolbar>
+
+      <md-list @click.native="closeLeftSidenav">
+        <md-list-item>Perfil</md-list-item>
+        <md-list-item>
+          <router-link to="/categories">Categorias</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/products">Produtos</router-link>
+        </md-list-item>
+        <md-list-item>Sair</md-list-item>
+      </md-list>
+   </md-sidenav>
+
+   <md-row>
+     <router-view></router-view>
+   </md-row>
+  </main>
 </template>
 
 <script>
-  export default {
-    name: 'testeComponent'
+export default {
+  name: 'WebschoolComponent',
+  data () {
+    return {
+      title: 'Ecommerce Webschool'
+    }
+  },
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    closeLeftSidenav () {
+      this.$refs.leftSidenav.close()
+    },
+    open (ref) {
+      console.log('Menu aberto: ' + ref)
+    },
+    close (ref) {
+      console.log('Menu fechado: ' + ref)
+    }
   }
-
+}
 </script>
 
 <style>
-
 </style>
